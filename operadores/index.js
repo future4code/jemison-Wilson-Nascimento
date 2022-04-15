f0c7aa50f67d956790485bd8b921bc669e599f63
Wilson Nascimento Alves <virsaoalves@gmail.com>
2022-04-14 21:08:43 -0300

    ecercicio-operadores

---
+++ operadores/index.js
@@ -0,0 +1,12 @@
+/* 1) Resposta a. false, pois um dos 2 é false
+Resposta b. false, pois um dos 3 é false
+Resposta c. true, pois os 2 deram true
+resposta d. boolean, pois é uma variável true*/
+/*2) Ele não identificou para trasnformar o prompt em number, irá imprimir primeiroNumero + segundoNumero*/
+/*3)
+let primeiroNumero = number (prompt("Digite um numero!"))
+let segundoNumero = number (prompt("Digite outro numero!"))
+
+const soma = primeiroNumero + segundoNumero
+
+console.log(soma)*/
