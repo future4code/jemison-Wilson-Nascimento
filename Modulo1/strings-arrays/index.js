d28da00768b53e1af91548815e42a0eb235aa734
Wilson Nascimento Alves <virsaoalves@gmail.com>
2022-04-19 22:23:22 -0300

    exercicio-strings-arrays

---
+++ Modulo1/strings-arrays/index.js
@@ -0,0 +1,79 @@
+/* Respostas
+Exercicio 1
+a. undefinied 
+b. null
+c. 11
+d. 3
+e. Array(11) [ 3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+f.9
+Exercicio 2
+SUBI NUM ONIBUS EM MIRROCOS, 27
+*/
+
+/*let nomeDoUsuario
+let emailDoUsuario
+nomeDoUsuario = prompt("Qual seu nome")
+emailDoUsuario = prompt("Qual seu email")
+let frase = (`O Email ${emailDoUsuario} foi cadastrado com sucesso. Seja-vinda(o), ${nomeDoUsuario}!`)
+console.log(frase)*/
+
+let array = ['TEMAKI', 'LASANHA', 'TAPIOCA', 'HAMBURGUER', 'ESFIHA']
+//console.log(array)
+//array = ['TEMAKI\n LASANHA\n TAPIOCA\n HAMBURGUER\n ESFIHA']
+//let frase = (`Essas são minhas comidas preferidas:\n ${array}`)
+//console.log(frase)
+let comidaFavorita = prompt('Qual sua comida favorita')
+let novaLista = array.replaceAll('LASANHA', ${comidaFavorita})
+console.log(novaLista)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
