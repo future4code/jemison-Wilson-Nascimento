431a557f3c746c4d6dbaae859267a9a00bc0993d
Wilson Nascimento Alves <virsaoalves@gmail.com>
2022-04-22 19:14:21 -0300

    exercicio-funcoes

---
+++ Modulo1/funcoes/index.js
@@ -0,0 +1,50 @@
+/* Respostas
+Exercicios de interpretação
+1.a) 10
+50  
+b) Apareceria nada
+2.a) Irá procurar se no texto do usuário possui a palavra cenoura
+b) I. true
+II. true
+III. false
+*/
+
+// Exercicios de escrita
+/*1.a)
+function imprimeFrase(){
+   console.log('Eu sou Wilson, tenho 34 anos, moro em São Paulo e sou estudante.')
+}
+imprimeFrase()*/
+
+/*b)
+function imprimeFrase(Nome, Idade, Cidade, Profissão){
+    console.log(`Eu sou ${Nome}, tenho ${Idade} anos, moro em ${Cidade} e sou ${Profissão}.`)
+ }
+ imprimeFrase('Wilson', 34, 'São Paulo', 'estudante')*/
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
