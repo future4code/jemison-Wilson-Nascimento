2577046310932e6b602060d6b267e5a6372c6fc8
Wilson Alves <virsaoalves@gmail.com>
2022-04-28 23:28:32 -0300

    exercicios-condicionais

---
+++ modulo1/condicionais/index.js
@@ -0,0 +1,19 @@
+/*RESPOSTAS
+Exercicios de interpretação
+
+1.a) Se o resto entre numero dado pelo usuario e 2 for igual a 0, imprime Passou no teste, se não, imprime Não passou no teste.
+b) Os numeros que resultam em resto = 0
+c) Para os numeros que resultam em um resto diferente de 0
+
+2.a) Para identificar o preço de uma fruta
+b) O preço da fruta Maça é R$ 2.25
+c) O preço da fruta  Pêra  é  R$  5.5
+
+3.a) Esta pedindo um numero para o usuário
+b) Imprimi esse numero passou no teste. Não imprimi nada
+c) Não
+
+*/
+
+
+
