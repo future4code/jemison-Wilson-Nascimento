08e2ecb008d6489e0541cabd764c6cced1f3a589
Wilson Alves <virsaoalves@gmail.com>
2022-05-03 22:46:06 -0300

    laco

---
+++ modulo1/lacos/index.js
@@ -0,0 +1,35 @@
+/* RESPOSTAS
+Exercicios de interpretação 
+
+1. 10
+
+2.a) Os numeros maiores que 18
+b) Sim, apagando a linha if (numero > 18)
+
+3.
+*
+**
+***
+****
+
+*/
+
+// Exercicios de escrita
+
+//1.
+const quantidadeBichos = Number(prompt("Quantos bichos de estimação você tem?"))
+if(quantidadeBichos == 0){
+console.log("Que pena! Você pode adotar um pet!")
+} while(quantidadeBichos > 0);{
+    let arrayAnimais = prompt('Digite o nome do primeiro Animal')
+arrayAnimais ++
+}
+
+
+//2.
+/*const arrayOriginal [1,2,3,4,5]
+for(numeros of arrayOriginal){
+    console.log(numeros)
+
+}
+*/
