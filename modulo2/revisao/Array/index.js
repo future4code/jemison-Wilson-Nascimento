50d712be3ec32f5a4de0e198b83344d14c80aeea
Wilson Alves <virsaoalves@gmail.com>
2022-06-06 22:15:55 -0300

    revisao-array-exercicio2

--- modulo2/revisao/Array/index.js
+++ modulo2/revisao/Array/index.js
@@ -1,4 +1,31 @@
-const racasCachorros = ["Vira Lata", "Boxer", "Golden", "Rottweiler", "Doberman"]
+//RESPOSTAS
+
+//1.Crie um array com pelo menos 5 raças de cachorro.Cada raça é um item diferente do array..
+
+/*const racasCachorros = ["Vira Lata", "Boxer", "Golden", "Rottweiler", "Doberman"]
 let raca = prompt("Usuário, escolha um numero de 1 a 5")
-console.log(racasCachorros[raca])
+console.log(racasCachorros[raca])*/
+
+//2. Para este exercício começe criando um array com os valores: 1,2,3,4,5 e 6
+
+let array = [1,2,3,4,5,6] 
+
+//--Determine o tamanho do array--
+
+//console.log(array.length)
+
+//--adicione o número 7--
+
+//array.push(7)
+//console.log(array)
+
+//--Remova os números 4 e 5.--
+
+array.splice(3,2)
+//console.log(array)
+
+
+//--Determine o novo tamanho do array--
+
+console.log(array.length)
 
